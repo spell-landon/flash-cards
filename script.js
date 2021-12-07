768e3449ecbd65bb74176f97246dfc1f10d92703
spell-landon <spell.landon@gmail.com>
2021-12-07 14:40:04 -0600

    Redesign html with form. Add functionality to arrow buttons

--- script.js
+++ script.js
@@ -30,9 +30,16 @@ const startPageEl = document.querySelector('.game-start-page');
 const scoreSection = document.querySelector('.score-section');
 const gameBoardSection = document.querySelector('.game-board');
 const cardCounter = document.querySelector('.card-counter');
+const formEl = document.querySelector('form');
+const cardEl = document.querySelector('#card-text');
+const nextArrow = document.querySelector('.fa-arrow-right');
+const prevArrow = document.querySelector('.fa-arrow-left');
 
 /*----- event listeners -----*/
 startBtn.addEventListener('click', startGame);
+formEl.addEventListener('submit', checkAnswer);
+nextArrow.addEventListener('click', nextQuestion);
+prevArrow.addEventListener('click', previousQuestion);
 
 /*----- functions -----*/
 function startGame() {
@@ -42,57 +49,75 @@ function startGame() {
   scoreSection.classList.remove('hidden');
   // Show the gameplay section
   gameBoardSection.classList.remove('hidden');
+  // Set card innerText to first question
+  cardEl.innerText = QUESTIONS[i];
+  cardCounter.innerText = `${i + 1}/10`;
   // Set the cardCounter innerText
-  cardCounter.innerText = `1/10`;
+  //   cardCounter.innerText = `1/10`;
   // Add the cardCounter to the game board section
-  gameBoardSection.append(cardCounter);
+  //   gameBoardSection.append(cardCounter);
   // Add a card to the game board section
-  createCard_Buttons();
+  //   createCard_Buttons();
 }
 
-function createCard() {
-  // create a div called card
-  const card = document.createElement('div');
-  // add the styling class of "card" to the div
-  card.setAttribute('class', 'card');
-  // create a 'p' element to contain the card text
-  const cardTextEl = document.createElement('p');
-  // append that text element to the card
-  card.appendChild(cardTextEl);
-  // add text styling to inner text on card
-  cardTextEl.setAttribute('id', 'card-text');
-  //* Finish the logic behind cycling through each question when the next button is clicked
-  cardTextEl.innerText = QUESTIONS[0];
-  // add card to the game board section
-  return card;
+// function createCard() {
+//   // create a div called card
+//   const card = document.createElement('div');
+//   // add the styling class of "card" to the div
+//   card.setAttribute('class', 'card');
+//   // create a 'p' element to contain the card text
+//   const cardTextEl = document.createElement('p');
+//   // append that text element to the card
+//   card.appendChild(cardTextEl);
+//   // add text styling to inner text on card
+//   cardTextEl.setAttribute('id', 'card-text');
+//   //* Finish the logic behind cycling through each question when the next button is clicked
+//   cardTextEl.innerText = QUESTIONS[1];
+//   // add card to the game board section
+//   return card;
+// }
+// function createArrowButton(direction) {
+//   // create a button for the previous button
+//   const arrowButton = document.createElement('button');
+//   // create an 'i' element to hold the font-awesome icon
+//   const fontAwesomeArrowIcon = document.createElement('i');
+//   // add the previous arrow attributes to 'i' element
+//   fontAwesomeArrowIcon.setAttribute('class', `fas fa-arrow-${direction}`);
+//   // add the id arrowButton to arrow
+//   arrowButton.setAttribute('id', 'arrowBtn');
+//   // attach the fontAwesomeArrowIcon to the arrowButton
+//   arrowButton.appendChild(fontAwesomeArrowIcon);
+//   // attach the arrowButton to the game section
+//   return arrowButton;
+// }
+// function createCard_Buttons() {
+//   // create section for the card and buttons on the game board
+//   const cardAndBtnSection = document.createElement('section');
+//   // append cardAndBtnSection to the game board
+//   gameBoardSection.append(cardAndBtnSection);
+//   // append the prev btn and create card functions
+//   cardAndBtnSection.append(createArrowButton(`left`));
+//   cardAndBtnSection.append(createCard());
+//   cardAndBtnSection.append(createArrowButton(`right`));
+//   // set attributes for the card and button section
+//   cardAndBtnSection.setAttribute('class', 'card-and-buttons');
+// }
+
+function checkAnswer(event) {
+  event.preventDefault();
+  console.log('Youre checking the answer');
 }
-function createArrowButton(direction) {
-  // create a button for the previous button
-  const arrowButton = document.createElement('button');
-  // create an 'i' element to hold the font-awesome icon
-  const fontAwesomeArrowIcon = document.createElement('i');
-  // add the previous arrow attributes to 'i' element
-  fontAwesomeArrowIcon.setAttribute('class', `fas fa-arrow-${direction}`);
-  // add the id arrowButton to arrow
-  arrowButton.setAttribute('id', 'arrowBtn');
-  // attach the fontAwesomeArrowIcon to the arrowButton
-  arrowButton.appendChild(fontAwesomeArrowIcon);
-  // attach the arrowButton to the game section
-  return arrowButton;
+function nextQuestion() {
+  if (i < 9) {
+    i++;
+    cardEl.innerText = QUESTIONS[i];
+    cardCounter.innerText = `${i + 1}/10`;
+  }
 }
-function createCard_Buttons() {
-  // create section for the card and buttons on the game board
-  const cardAndBtnSection = document.createElement('section');
-  // append cardAndBtnSection to the game board
-  gameBoardSection.append(cardAndBtnSection);
-  // append the prev btn and create card functions
-  cardAndBtnSection.append(createArrowButton(`left`));
-  cardAndBtnSection.append(createCard());
-  cardAndBtnSection.append(createArrowButton(`right`));
-  // set attributes for the card and button section
-  cardAndBtnSection.setAttribute('class', 'card-and-buttons');
-}
-function setQuestions() {
-  // create a section for the questions
-  const questionSection = document.createElement('section');
+function previousQuestion() {
+  if (i !== 0) {
+    i--;
+    cardEl.innerText = QUESTIONS[i];
+    cardCounter.innerText = `${i + 1}/10`;
+  }
 }
